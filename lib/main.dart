a9c780341f556587fe07cda45b7c0ae414b994a4
Dipin KP <dipinpnambiar@gmail.com>
2020-07-18 12:08:41 +0530

    mediaquery responsive

--- lib/main.dart
+++ lib/main.dart
@@ -98,22 +98,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    //final curScaleFactor = MediaQuery.of(context).textScaleFactor;
+    final appBar = AppBar(
+      title: Text('Expense Finder', style: TextStyle(fontFamily: 'OpenSans')),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context))
+      ],
+    );
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Expense Finder', style: TextStyle(fontFamily: 'OpenSans')),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => _startAddNewTransaction(context))
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTxns),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.4,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.6,
+              child: TransactionList(_userTransactions, _deleteTxns),
+            ),
             /* Transform(
               transform: Matrix4.skewX(0.1),
               child: TransactionList(_userTransactions),
